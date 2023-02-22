a516e0f62dbcc5d9503ab04650ac9daf8dd5fa28
Franklin Souza <frankrsouza@gmail.com>
2023-02-22 20:14:56 -0300

    Feat: Adiciona a função de expandir e recolher filtro por estado

--- src/pages/Home/components/Filter/index.jsx
+++ src/pages/Home/components/Filter/index.jsx
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import styles from './styles.module.scss';
 
 const Filter = ({states, checkState}) => {
+  const [formStatus, setFormStatus] = useState(false);
 
   const checkHandler = (id) => {
     checkState(id);
   };
 
+  const handleFormStatus = () => {
+    setFormStatus(!formStatus);
+  }
+  
+  console.log(formStatus);
+
   return (
     <aside className={styles.filter}>
       <h3 className={styles.filter__title}>Por Estado</h3>
-      <form>
+      <form className={`${styles['filter__form']} ${formStatus ? styles['filter__form--open'] : ''}`}>
         {states.map((item) =>
           <div className={styles['filter__line-form']} key={item.id}>
             <input 
@@ -24,7 +32,9 @@ const Filter = ({states, checkState}) => {
           </div>
         )}
       </form>
-      <span>Ver todos</span>
+      <span onClick={handleFormStatus}>
+        {formStatus ? 'Recolher' : 'Ver todos'}
+      </span>
     </aside>
   )
 }
